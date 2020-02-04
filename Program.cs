6fb909d9dc811b314ca98ffa0dcc3d2c4187b359
yakovsmonarch <medvejonok07gmail.com>
2020-02-04 14:15:26 +0800

    Parsing fen file in board

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessEngineCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fen = Console.ReadLine();
+            string[] temp = fen.Split();
+            string[] board = temp[0].Split('/');
+            Console.WriteLine("  +-----------------+");
+            for(int i = 0; i < board.Length; i++)
+            {
+                Char[] fig = board[i].ToCharArray();
+                Console.Write("{0} {1}", board.Length-i, "| ");
+                for(int j = 0; j < fig.Length; j++)
+                {
+                    int num;
+                    if (int.TryParse(fig[j].ToString(), out num) == true)
+                    {
+                        for (int pointEmpty = 0; pointEmpty < num; pointEmpty++)
+                        {
+                            Console.Write(". ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(fig[j] + " ");
+                    }
+                    
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            Console.WriteLine("  +-----------------+");
+            Console.Write("    ");
+            for(char latChar = 'a'; latChar <= 'h'; latChar++)
+            {
+                Console.Write(latChar + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
