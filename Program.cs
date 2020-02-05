3b83fd35cb63ee74c662514964e2d0d0b6b6d021
yakovsmonarch <medvejonok07gmail.com>
2020-02-05 13:33:24 +0800

    Refactoring

--- Program.cs
+++ Program.cs
@@ -10,41 +10,52 @@ namespace ChessEngineCSharp
     {
         static void Main(string[] args)
         {
-            string fen = Console.ReadLine();
+            Console.WriteLine(PrintFen(Console.ReadLine()));
+            Console.ReadLine();
+        }
+
+        private static string PrintFen(string fen)
+        {
+            StringBuilder sb = new StringBuilder();
             string[] temp = fen.Split();
             string[] board = temp[0].Split('/');
-            Console.WriteLine("  +-----------------+");
-            for(int i = 0; i < board.Length; i++)
+            sb.Append("  +-----------------+" + Environment.NewLine);
+
+            for (int i = 0; i < board.Length; i++)
             {
                 Char[] fig = board[i].ToCharArray();
-                Console.Write("{0} {1}", board.Length-i, "| ");
-                for(int j = 0; j < fig.Length; j++)
+                sb.Append((board.Length - i).ToString() + " | ");
+                for (int j = 0; j < fig.Length; j++)
                 {
                     int num;
                     if (int.TryParse(fig[j].ToString(), out num) == true)
                     {
                         for (int pointEmpty = 0; pointEmpty < num; pointEmpty++)
                         {
-                            Console.Write(". ");
+                            sb.Append(". ");
                         }
                     }
                     else
                     {
-                        Console.Write(fig[j] + " ");
+                        sb.Append(fig[j] + " ");
                     }
-                    
+
                 }
-                Console.Write("|");
-                Console.WriteLine();
+                sb.Append("|" + Environment.NewLine);
             }
-            Console.WriteLine("  +-----------------+");
-            Console.Write("    ");
-            for(char latChar = 'a'; latChar <= 'h'; latChar++)
+            sb.Append("  +-----------------+" + Environment.NewLine);
+            sb.Append("    ");
+            for (char latChar = 'a'; latChar <= 'h'; latChar++)
             {
-                Console.Write(latChar + " ");
+                sb.Append(latChar + " ");
             }
 
-            Console.ReadKey();
+            return sb.ToString();
+        }
+
+        private static ulong FigureToNum(int n)
+        {
+            return (ulong)Math.Pow(2, n);
         }
     }
 }
